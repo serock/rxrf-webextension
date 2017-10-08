b668ad66b03b5400a46467139d08e9d4fb848450
John Serock <1468970+serock@users.noreply.github.com>
2017-10-08 17:07:41 -0400

    Add files via upload

---
+++ rxrf-bg.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var regExps = {
+  hosts:   new RegExp("^$"),
+  tpHosts: new RegExp("^$")
+};
+
+function beforeRequestListener(details) {
+  var url;
+  try {
+    url = new URL(details.url);
+  } catch (e) {
+    console.error("[X]: " + details.url);
+    return {cancel: true};
+  }
+  if (url.protocol == "https:") {
+    if (regExps.hosts.test(url.hostname) || regExps.tpHosts.test(url.hostname)) {
+      //console.info(url.hostname);
+      return {cancel: false};
+    } else {
+      console.info("[X]: " + url.href);
+    }
+  } else if (url.protocol == "data:") {
+    console.info("[ ]: " + url.href);
+    return {cancel: false};
+  } else {
+    console.warn("[X]: " + url.href);
+  }
+  return {cancel: true};
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  beforeRequestListener,
+  {urls: ["<all_urls>"]},
+  ["blocking"]
+);
+
